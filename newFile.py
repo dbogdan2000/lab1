4fde6da1942a9660fedd8d3a93797a3127e816b2
Dmitriy Bogdan <dimabogdan2000@gmail.com>
2020-05-18 14:10:01 +0300

    added graphs

--- newFile.py
+++ newFile.py
@@ -16,19 +16,12 @@ class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-model.summary()
-
-model.add(layers.Flatten())
+model.add(layers.Flatten(input_shape = (32, 32, 3)))
+model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dense(128, activation='relu'))
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
+model.add(layers.Dense(32))
 
 model.summary()
 
@@ -40,7 +33,7 @@ log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-model.fit(train_images, train_labels, epochs=13,
+model.fit(train_images, train_labels, epochs=40,
                     validation_data=(test_images, test_labels), callbacks=[tensorboard_callback])
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
